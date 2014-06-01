dcecd048d3f4a7c7756aa65bdf19eff1f3012f06
ecaradec <emmanuel.caradec@gmail.com>
2014-06-01 18:24:17 +0200

    remove object from current selection

--- launcher/CurrentSelectionSource.h
+++ launcher/CurrentSelectionSource.h
@@ -25,7 +25,6 @@ CString getExplorerSelection(HWND hwndFind);
 struct CurrentSelectionSource : Source {
     CurrentSelectionSource() : Source(L"FILE", L"Current selection (Catalog )") {
         m_ignoreemptyquery=true;
-        m_index[L"CurrentSelection"]=SourceResult(new Object(L"CurrentSelection", L"Current Selection", this, L"Current Selection"));
     }
     ~CurrentSelectionSource() {
     }
@@ -34,20 +33,19 @@ struct CurrentSelectionSource : Source {
             return;
 
         CString q(query); q.MakeUpper();
-        for(std::map<CString, SourceResult>::iterator it=m_index.begin(); it!=m_index.end();it++) {
-            if(FuzzyMatch(it->second.display(),q)) {            
-                uint8 *pobj=pack.beginBlock();
-                pack.pack(L"type",L"FILE");
-                pack.pack(L"source",(uint32)this);
-                pack.pack(L"key",it->second.display());
-                pack.pack(L"path",getExplorerSelection(g_foregroundWnd));
-                pack.pack(L"expand",it->second.display());
-                pack.pack(L"filename",it->second.display());
-                pack.pack(L"text",it->second.display());
-                pack.pack(L"bonus",(uint32)0);
-                pack.pack(L"uses",(uint32)0);
-                pack.endBlock(pobj);
-            }
+        
+        if(FuzzyMatch(L"Current Selection",q)) {            
+            uint8 *pobj=pack.beginBlock();
+            pack.pack(L"type",L"FILE");
+            pack.pack(L"source",(uint32)this);
+            pack.pack(L"key",L"Current Selection");
+            pack.pack(L"path",getExplorerSelection(g_foregroundWnd));
+            pack.pack(L"expand",L"Current Selection");
+            pack.pack(L"filename",L"Current Selection");
+            pack.pack(L"text",L"Current Selection");
+            pack.pack(L"bonus",(uint32)0);
+            pack.pack(L"uses",(uint32)0);
+            pack.endBlock(pobj);
         }
     }
 };
